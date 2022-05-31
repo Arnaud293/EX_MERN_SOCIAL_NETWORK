283accef830f76a973ff49e95c73bce7fbce05f7
Arnaud293 <arnaudlt293@gmail.com>
2022-05-31 23:37:06 +0200

    MAJ roads on app

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const { json } = require('express');
 const express = require('express');
 const cookieParser = require('cookie-parser');
+
 const userRoutes = require('./routes/user.routes');
+const postRoutes = require('./routes/post.routes');
+
 require('dotenv').config({ path: './config/.env' });
 require('./config/db');
 const { checkUser, requireAuth } = require('./middleware/auth.middleware')
@@ -24,6 +27,7 @@ app.get('/jwtid', requireAuth, (req, res) => {
 
 // roads
 app.use('/api/user', userRoutes);
+app.use('/api/post', postRoutes);
 
 // Server
 app.listen(process.env.PORT, () => {
