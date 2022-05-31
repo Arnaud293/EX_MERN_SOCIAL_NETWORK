9d686faac2b19925e3fe6853084ae614d58842d4
Arnaud293 <arnaudlt293@gmail.com>
2022-05-31 18:01:00 +0200

    add cookie parser and checkUser on every req.get, add requiereAuth

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 const { json } = require('express');
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const userRoutes = require('./routes/user.routes');
 require('dotenv').config({ path: './config/.env' });
 require('./config/db');
+const { checkUser, requireAuth } = require('./middleware/auth.middleware')
 const app = express();
 
 
 
 app.use(express.json());
+app.use(cookieParser());
 // app.use(express.urlencoded({ extended: false }));
 
 
+// JWT
+app.get('*', checkUser);
+app.get('/jwtid', requireAuth, (req, res) => {
+    res.status(200).send(res.locals.user._id)
+});
 
 
 
