8b0716bdc8f1e95f7284a93199a0fa5fc6ca73ab
Arnaud293 <arnaudlt293@gmail.com>
2022-05-31 12:09:37 +0200

    fix issue with unexpected token in JSON

--- models/user.model.js
+++ models/user.model.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema(
     {
         pseudo: { type: String, required: true, minlength: 3, maxlength: 55, unique: true, trim: true },
         email: { type: String, required: true, unique: true, validate: [isEmail], lowercase: true, trim: true },
-        password: { type: String, required: true, max: 1024, min: 6, },
+        password: { type: String, required: true, max: 1024, minlength: 6 },
         bio: { type: String, max: 1024 },
         followers: { type: [String] },
         following: { type: [String] },
@@ -15,3 +15,6 @@ const userSchema = new mongoose.Schema(
         timestamps: true,
     }
 )
+
+const UserModel = mongoose.model('user', userSchema);
+module.exports = UserModel;
--- server.js
+++ server.js
@@ -1,8 +1,23 @@
+const { json } = require('express');
 const express = require('express');
+const userRoutes = require('./routes/user.routes');
 require('dotenv').config({ path: './config/.env' });
 require('./config/db');
 const app = express();
 
+
+
+app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+
+
+
+
+
+// roads
+app.use('/api/user', userRoutes);
+
+// Server
 app.listen(process.env.PORT, () => {
     console.log(`listening on port ${process.env.PORT}`);
 })
