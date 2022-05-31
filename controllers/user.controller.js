ba9f0ba9a211d36d15cd7326a59b113b5e0d749f
Arnaud293 <arnaudlt293@gmail.com>
2022-05-31 13:35:16 +0200

    add req get userInfo to display one user later

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -5,3 +5,17 @@ module.exports.getAllUsers = async (req, res) => {
     const users = await UserModel.find().select('-password');
     res.status(200).json(users);
 }
+
+module.exports.userInfo = async (req, res) => {
+    console.log(req.params);
+    if (!ObjectID.isValid(req.params.id))
+        return res.status(400).send('ID Unknown :' + req.params.id)
+    UserModel.findById(req.params.id, (error, docs) => {
+        if (!error) {
+            res.send(docs);
+        }
+        else {
+            console.log('Id Unknown :' + error);
+        }
+    }).select('-password');
+}
