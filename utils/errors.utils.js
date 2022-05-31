f0c62ef82a96a9c41e892957b77f2712da1d17d2
Arnaud293 <arnaudlt293@gmail.com>
2022-05-31 19:57:53 +0200

    Error management for auth is done

--- utils/errors.utils.js
+++ utils/errors.utils.js
@@ -7,7 +7,22 @@ module.exports.signUpErrors = (error) => {
         errors.email = 'Email incorrect ou déjà pris.';
     if (error.message.includes('password'))
         errors.password = 'Votre mot de passe doit contenir au minimum 6 caractères';
-    if (error.code === 11000)
+    if (error.code === 11000 && Object.keys(error.keyValue)[0].includes('email'))
         errors.email = 'Cet email est déjà associé à un compte';
+    if (error.code === 11000 && Object.keys(error.keyValue)[0].includes('pseudo'))
+        errors.email = 'Ce pseudo est déjà associé à un compte';
+
+
+    return errors;
+}
+
+module.exports.signInErrors = (error) => {
+    let errors = { email: '', password: '' }
+
+    if (error.message.includes('email'))
+        errors.email = 'Email inconnu';
+    if (error.message.includes('password'))
+        errors.password = 'Mot de passe incorrect';
+
     return errors;
 }
