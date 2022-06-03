06567a7c1402b10f571319ae97baeb48753b8564
Arnaud293 <arnaudlt293@gmail.com>
2022-06-03 14:02:06 +0200

    login and error msg OK

--- client/src/pages/Profil.js
+++ client/src/pages/Profil.js
@@ -1,15 +1,15 @@
 import React from "react";
-import Log from '../components/Log/index';
+import Log from "../components/Log/index";
 
 const Profil = () => {
-    return (
-        <div className="profil-page">
-            <div className="log-container">
-                <Log signin={false} signup={true}/>
-                <img src="./img/log.svg" alt="img-log" />
-            </div>
-        </div>
-    );
+  return (
+    <div className="profil-page">
+      <div className="log-container">
+        <Log signin={false} signup={true} />
+        <img src="./img/log.svg" alt="img-log" />
+      </div>
+    </div>
+  );
 };
 
 export default Profil;
