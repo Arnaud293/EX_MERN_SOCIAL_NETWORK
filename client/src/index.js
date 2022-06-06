d23c5b339b755080bc4ce62cb222cffd7bee37eb
Arnaud293 <arnaudlt293@gmail.com>
2022-06-06 18:40:58 +0200

    MAJ

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './styles/index.scss';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./styles/index.scss";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { applyMiddleware, legacy_createStore } from "redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import rootReducer from "./reducers";
+
+// const { createStore } = "redux";
+
+const store = legacy_createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk, logger))
 );
 
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
