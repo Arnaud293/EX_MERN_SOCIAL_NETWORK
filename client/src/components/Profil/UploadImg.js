d95d10b0baddc3ed39ae2566ac1368e5a533622b
Arnaud293 <arnaudlt293@gmail.com>
2022-06-07 11:18:19 +0200

    Add upload profil img

--- client/src/components/Profil/UploadImg.js
+++ client/src/components/Profil/UploadImg.js
@@ -1,10 +1,21 @@
-import React, { useState } from "react";
+import React, { useReducer, useState } from "react";
+import { uploadPicture } from "../../actions/user.actions";
+import { useDispatch, useSelector } from "react-redux";
 
 const UploadImg = () => {
   const [file, setFile] = useState();
+  const dispatch = useDispatch();
+  const userData = useSelector((state) => state.userReducer);
 
   const handlePicture = (e) => {
     e.preventDefault();
+
+    const data = new FormData();
+    data.append("name", userData.pseudo);
+    data.append("userId", userData._id);
+    data.append("file", file);
+
+    dispatch(uploadPicture(data, userData._id));
   };
 
   return (
@@ -17,6 +28,8 @@ const UploadImg = () => {
         accept=".jpg, .jpeg, .png"
         onChange={(e) => setFile(e.target.files[0])}
       />
+      <br />
+      <input type="submit" value="Envoyer" />
     </form>
   );
 };
